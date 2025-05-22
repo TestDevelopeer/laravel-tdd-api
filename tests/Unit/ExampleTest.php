8ff4425231943a7141f69025960d8a49943d2244
yessir <tdevelopeer@gmail.com>
2025-05-22 10:21:37 +0300

    примера unit

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Support\Str;
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
@@ -9,8 +11,15 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+    #[Test]
+    public function that_true_is_true(): void
     {
         $this->assertTrue(true);
+    }
+
+    #[Test]
+    public function is_string_returned_of_name_method(): void {
+        $str = Str::random(12);
+        $this->assertIsString($str);
     }
 }
